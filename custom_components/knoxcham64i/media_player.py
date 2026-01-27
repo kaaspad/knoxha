50489419f47196dc9f14e509a6451f3126398470
kaaspad <kaaspad@gmail.com>
2026-01-27 16:48:04 +0200

    Bump version to 1.1.2 - Speaker icon

--- custom_components/knoxcham64i/media_player.py
+++ custom_components/knoxcham64i/media_player.py
@@ -343,7 +343,7 @@ class ChameleonMediaPlayer(CoordinatorEntity, MediaPlayerEntity, RestoreEntity):
             "zone_name": self._zone_name,
             "knox_zone_id": self._zone_id,
             "knox_volume_raw": zone_state.volume if zone_state else None,
-            "integration_version": "1.1.1",
+            "integration_version": "1.1.2",
         }
 
         # Add input_id if available
